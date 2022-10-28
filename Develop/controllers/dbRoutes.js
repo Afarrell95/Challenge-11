2664eff34720047c0ad5dda2e61c81cf6ad01aef
Allison <farrella95@gmail.com>
2022-10-28 13:35:58 -0400

    imgonna mcfreaking lose it

--- Develop/controllers/dbRoutes.js
+++ Develop/controllers/dbRoutes.js
@@ -2,22 +2,22 @@ const router = require("express").Router();
 
 const db = require("../db/db.json");
 
-let notes = json.parse(data);
-
 router.get("/notes", (req, res) => {
-  res.json(notes);
+  db.getNotes().then((notes) => {
+    return res.json(notes);
+  });
 });
 
 router.post("/notes", (req, res) => {
   let newNote = req.body;
-  notes.push(newNote);
+  db.push(newNote);
   saveNote().then((note) => {
-    res.json(note);
+    return res.json(note);
   });
 });
 
 router.delete("/notes/:id", (req, res) => {
-  notes.splice(req.params.id, 1);
+  db.splice(req.params.id, 1);
   updateDb();
   console.log("Deleted note with id " + req.params.id);
   res.json("Note has been deleted!");
